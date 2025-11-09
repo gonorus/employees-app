8c44e6bd097dd3200db415036ad0c3a1425172dd
Dwi Anggono W.S.P. <casperkepalabundar@gmail.com>
2025-11-10 00:32:23 +0700

    feat: define base style for the application

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@assets': path.resolve(__dirname, './src/assets'),
+      '@styles': path.resolve(__dirname, './src/styles'),
       '@organisms': path.resolve(__dirname, './src/components/organisms'),
     },
   },
